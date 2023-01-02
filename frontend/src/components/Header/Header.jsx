998651254f44f3aa05ffdb3949ea7ecfe68db0f2
Mat2dr <dragovic.mathias@gmail.com>
2023-01-02 21:38:42 +0100

    Starting the frontend with the landing page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import agentBankLogo from '../../img/argentBankLogo.png'
+
+
+const Header = () => {
+  return (
+    <div>
+        <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+            <img className="main-nav-logo-image" src={agentBankLogo} alt="Argent Bank Logo"/>
+            <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+            <a className="main-nav-item" href="./sign-in.html">
+                <i className="fa fa-user-circle"></i>Sign In
+            </a>
+        </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
