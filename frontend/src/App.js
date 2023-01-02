43429b2d7195c186e55c9570d37d42106e83491d
Mat2dr <dragovic.mathias@gmail.com>
2023-01-02 21:54:47 +0100

    Added react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Login from './pages/Login/Login';
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header/>
     <main>
-      <LandingPage/>
+      <Routes>
+        <Route path='/' element={<LandingPage />} exact/>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </main>
     <Footer/>
-  </>
+  </BrowserRouter>
 )
 
 export default App;
